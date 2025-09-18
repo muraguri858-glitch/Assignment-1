6d2de34ebd069f75c32d74f97cad1bcc479dc10f
muraguri858-glitch <muraguri858@gmail.com>
2025-09-18 19:00:01 +0300

    Submission of assignments

---
+++ assignment 2.c
@@ -0,0 +1,34 @@
+//simple c program (hae guys), double line comment
+/*
+name: Samuel muraguri 
+reg number:CT100/G/26191/25
+Description:hello guys
+*/
+
+#include<stdio.h>
+
+//main function
+int main(){
+int height;
+char phonenumber[20];
+int weight;
+
+printf("enter your height:");
+scanf("%d",& height );
+
+printf("enter your phone number:");
+scanf("%d", phonenumber);
+
+printf("enter your weight:");
+scanf("%d",& weight);
+
+printf("\your height is %d cm\n", height);
+printf("\nyour phonenumber is %d\n", phonenumber);
+printf("your weight is %d kg\n", weight);
+return 0;
+}
+
+
+    
+    
+    
---
+++ assino.c
@@ -0,0 +1,20 @@
+//simple c program (hae guys), double line comment
+/*
+name: Samuel muraguri 
+reg number:CT100/G/26191/25
+Description:hello guys
+*/
+
+#include<stdio.h>
+
+//main function
+int main(){
+printf("This are the units that i am doing this semester \n");
+printf ("Analogue electronics, computer system and organisation, fundamental of ICT, introduction to computer applications, essential of accounting, communication skills, mathematics for science, introduction to programming and algorithms  \n");
+return 0;
+}
+
+
+    
+    
+    
